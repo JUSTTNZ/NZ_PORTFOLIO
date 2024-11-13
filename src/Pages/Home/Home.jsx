5c2991eb81388c8b304a73805b6916519ace1464
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-11-13 09:26:42 -0800

    done with the sidebar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@
 const Home = () => {
   return (
     <div className="absolute left-[12%]">
-      <h2 className="text-gray-700 text-3xl"></h2>
+      <h1>Hi</h1>
     </div>
   )
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { NavLink } from 'react-router-dom';
+import './Sidebar.css'
 import { AiOutlineHome, AiOutlineUser, AiOutlineFolderOpen, AiOutlineMail, AiOutlineClose,AiFillLinkedin, AiFillGithub, AiFillInstagram, AiOutlineMenu  } from 'react-icons/ai';
 import { FiBriefcase } from 'react-icons/fi';
 import { useState } from 'react';
@@ -12,39 +13,55 @@ import { useState } from 'react';
             <p className=' px-2 text-blue-400 text-2xl font-mono font-bold'>Confidence.</p>
             <div>
             <NavLink 
-          to={'/'} 
-          onClick={() => setActiveLink('/')}
-          className="flex flex-col items-center justify-center mt-2"
-        >
-          <div className={`flex items-center justify-center h-12 w-12 rounded-full transition-all duration-300 ease-linear cursor-pointer ${activeLink === '/' ? 'bg-blue-500' : 'bg-gray-800 hover:bg-blue-600'}`}>
-            <AiOutlineHome size="30" className="text-white" />
-          </div>
-          <span className='mt-1 text-gray-300 font-mono'>Home</span>
-        </NavLink>
-                <NavLink 
-                to={'/about'}
-              onClick={() => setActiveLink('/about')}
-          className="flex flex-col items-center justify-center mt-2">
-            <div className={`flex items-center justify-center h-12 w-12 rounded-full transition-all duration-300 ease-linear cursor-pointer ${activeLink === '/about' ? 'bg-blue-500' : ' hover:bg-blue-600'}`}>
+            to={'/'} 
+            onClick={() => setActiveLink('/')}
+            className="flex flex-col items-center justify-center mt-2">
+                <div className={`flex items-center justify-center h-12 w-12 rounded-full transition-all duration-300 ease-linear cursor-pointer ${activeLink === '/' ? 'bg-blue-500' : 'bg-gray-800 hover:bg-blue-600'}`}>
+                    <AiOutlineHome size="30" className="text-white" />
+                </div>
+                {activeLink === '/' && <div className='indicator'></div>}
+                <span className='mt-1 text-gray-300 font-mono'>Home</span>
+            </NavLink>
+            <NavLink 
+            to={'/about'}
+            onClick={() => setActiveLink('/about')}
+            className="flex flex-col items-center justify-center mt-2">
+                <div className={`flex items-center justify-center h-12 w-12 rounded-full transition-all duration-300 ease-linear cursor-pointer ${activeLink === '/about' ? 'bg-blue-500' : 'bg-gray-800 hover:bg-blue-600'}`}>
                     <AiOutlineUser size="30" className="text-white" />
-                    </div>
+                </div>
+                {activeLink === '/about' && <div className='indicator'></div>}
                     <span className='mt-1 text-gray-300 font-mono'>About</span>
-                </NavLink>
-                <NavLink 
-                to={'/work'}
-                className={({ isActive}) => `${isActive ? 'bg-blue-500' : 'bg-gray-800'}`}>
-                    <SideBarIcon icon={<FiBriefcase size={30}/>} name= 'Work'/>
-                </NavLink>
-                <NavLink 
-                to={'/portfolio'}
-                className={({ isActive}) => `${isActive ? 'bg-blue-500' : 'bg-gray-800'}`}>
-                    <SideBarIcon icon={<AiOutlineFolderOpen size={30}/>} name= 'Portfolio'/>
-                </NavLink>
-                <NavLink 
-                to={'/contact'}
-                className={({ isActive}) => `${isActive ? 'bg-blue-500' : 'bg-gray-800'}`}>
-                    <SideBarIcon icon={<AiOutlineMail size={30}/>} name= 'Contact'/>
-                </NavLink>
+            </NavLink>
+            <NavLink 
+            to={'/about'}
+            onClick={() => setActiveLink('/work')}
+            className="flex flex-col items-center justify-center mt-2">
+                <div className={`flex items-center justify-center h-12 w-12 rounded-full transition-all duration-300 ease-linear cursor-pointer ${activeLink === '/work' ? 'bg-blue-500' : 'bg-gray-800 hover:bg-blue-600'}`}>
+                    <FiBriefcase size="30" className="text-white" />
+                </div>
+                {activeLink === '/work' && <div className='indicator'></div>}
+                    <span className='mt-1 text-gray-300 font-mono'>Work</span>
+            </NavLink>
+            <NavLink 
+            to={'/about'}
+            onClick={() => setActiveLink('/portfolio')}
+            className="flex flex-col items-center justify-center mt-2">
+                <div className={`flex items-center justify-center h-12 w-12 rounded-full transition-all duration-300 ease-linear cursor-pointer ${activeLink === '/portfolio' ? 'bg-blue-500 ' : 'bg-gray-800 hover:bg-blue-600'}`}>
+                    <AiOutlineFolderOpen size="30" className="text-white" />
+                </div>
+                {activeLink === '/portfolio' && <div className='indicator'></div>}
+                    <span className='mt-1 text-gray-300 font-mono'>Portfolio</span>
+            </NavLink>
+            <NavLink 
+            to={'/about'}
+            onClick={() => setActiveLink('/contact')}
+            className="flex flex-col items-center justify-center mt-2">
+                <div className={`flex items-center justify-center h-12 w-12 rounded-full transition-all duration-300 ease-linear cursor-pointer ${activeLink === '/contact' ? 'bg-blue-500' :'bg-gray-800 hover:bg-blue-600'}`}>
+                    <AiOutlineMail size="30" className="text-white" />
+                </div>
+                {activeLink === '/contact' && <div className='indicator'></div>}
+                    <span className='mt-1 text-gray-300 font-mono'>Contact</span>
+            </NavLink>
             </div>
             <div>
             <SideBarLink icon={<AiFillLinkedin size={15}/>} name= 'Contact'/>
@@ -58,12 +75,6 @@ import { useState } from 'react';
   
 }
 
-const SideBarIcon = ({ icon, name }) => (
-    <div className=''>
-        <div className='sidebar-icon'>{icon}</div>
-        <span className='sidebar-names'>{name}</span>
-    </div>
-)
 
 const SideBarLink = ({icon}) => (
     <div>
