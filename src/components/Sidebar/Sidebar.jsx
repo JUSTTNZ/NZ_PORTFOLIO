5a8c6e9812dcf3506802dfb2cfb4e8fd2f6600b7
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-11-13 16:53:27 +0100

    dominic

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,23 +3,33 @@ import { NavLink } from 'react-router-dom';
 import './Sidebar.css'
 import { AiOutlineHome, AiOutlineUser, AiOutlineFolderOpen, AiOutlineMail, AiOutlineClose,AiFillLinkedin, AiFillGithub, AiFillInstagram, AiOutlineMenu  } from 'react-icons/ai';
 import { FiBriefcase } from 'react-icons/fi';
+import { useState } from 'react';
 
  const Sidebar = ({isActive}) => {
-    
+    const [activeLink, setActiveLink] = useState('/');
   return (
     <>
         <div className="fixed flex flex-col justify-around top-0 left-0 h-screen w-[-50px] m-0 bg-gray-900 text-white shadow-lg">
             <p className=' px-2 text-blue-400 text-2xl font-mono font-bold'>Confidence.</p>
             <div>
-                <NavLink 
-                to={'/'} 
-                className={({ isActive}) => `${isActive ? 'bg-blue-500' : 'bg-gray-800'}`}>
-                <SideBarIcon icon={<AiOutlineHome size="30"/>} name= 'Home' isActive={isActive}/>
-                </NavLink>
+            <NavLink 
+          to={'/'} 
+          onClick={() => setActiveLink('/')}
+          className="flex flex-col items-center justify-center mt-2"
+        >
+          <div className={`flex items-center justify-center h-12 w-12 rounded-full transition-all duration-300 ease-linear cursor-pointer ${activeLink === '/' ? 'bg-blue-500' : 'bg-gray-800 hover:bg-blue-600'}`}>
+            <AiOutlineHome size="30" className="text-white" />
+          </div>
+          <span className='mt-1 text-gray-300 font-mono'>Home</span>
+        </NavLink>
                 <NavLink 
                 to={'/about'}
-                className={({ isActive}) => `${isActive ? 'bg-blue-500' : 'bg-gray-800'}`}>
-                    <SideBarIcon icon={<AiOutlineUser size={30}/>} name= 'About' isActive={isActive}/>
+              onClick={() => setActiveLink('/about')}
+          className="flex flex-col items-center justify-center mt-2">
+            <div className={`flex items-center justify-center h-12 w-12 rounded-full transition-all duration-300 ease-linear cursor-pointer ${activeLink === '/about' ? 'bg-blue-500' : ' hover:bg-blue-600'}`}>
+                    <AiOutlineUser size="30" className="text-white" />
+                    </div>
+                    <span className='mt-1 text-gray-300 font-mono'>About</span>
                 </NavLink>
                 <NavLink 
                 to={'/work'}
