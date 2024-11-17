c294706691c6f20921df3aecc87def34cb2e81dc
CHUKWUDI CONFIDENCE OSINACHI <chukwudiconfidenceosinachi@gmail.com>
2024-11-17 13:55:34 +0100

    Update About.jsx

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
                         <AnimatedLetters letterClass={letterClass} strArray={"About me".split("")} idx={10}/>
                     </h1>
                     <p>
-                        I am a very ambitious software developer with a unique background that has prepared me to be a team player who communicates effectively, faces problems analytically, and has the technical skills to tackle any challenge. 
+                        I am a very ambitious frontend developer with a unique background that has prepared me to be a team player who communicates effectively, faces problems analytically, and has the technical skills to tackle any challenge. 
                         <br/>
                         <LanguageList />
                         <Link to="/about/tech" className="flat-button">FRAMEWORKS/TECHNOLOGIES</Link>
